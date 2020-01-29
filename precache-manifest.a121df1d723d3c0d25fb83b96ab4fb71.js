ca914b72020099e8c3a796614706658c81dbf860
Mariusz Buskiewicz <grajzesz@gmail.com>
2020-01-29 23:02:55 +0100

    Updates

---
+++ precache-manifest.a121df1d723d3c0d25fb83b96ab4fb71.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "01a6bbcaf32474ba01375289ea57d003",
+    "url": "/WeatherApi/static/media/rain.01a6bbca.jpg"
+  },
+  {
+    "revision": "d642f171fd8f1e81ef2d",
+    "url": "/WeatherApi/static/css/main.90dc5c0d.chunk.css"
+  },
+  {
+    "revision": "5a616593ab10ba6d39e8",
+    "url": "/WeatherApi/static/js/runtime~main.fb82031b.js"
+  },
+  {
+    "revision": "6b9c3b327f00c63313a9",
+    "url": "/WeatherApi/static/js/2.46c3ff79.chunk.js"
+  },
+  {
+    "revision": "324e4722f2b433c7538cb31eb0f229db",
+    "url": "/WeatherApi/static/media/night-broken-clouds.324e4722.jpg"
+  },
+  {
+    "revision": "7e4ee0861d6cf586f8bc7458b9fdd33b",
+    "url": "/WeatherApi/static/media/night-rain.7e4ee086.jpg"
+  },
+  {
+    "revision": "ebb71f41a71ebf11b75ebd3feefd3a88",
+    "url": "/WeatherApi/static/media/no-clouds.ebb71f41.jpg"
+  },
+  {
+    "revision": "ca67daa43f373544c427b02062df2965",
+    "url": "/WeatherApi/static/media/few-clouds.ca67daa4.jpg"
+  },
+  {
+    "revision": "3a4b8febb8263227207d4d4385d93cf5",
+    "url": "/WeatherApi/static/media/broken-clouds.3a4b8feb.jpg"
+  },
+  {
+    "revision": "02d4abaefc47dad9a1a8be6204ad60f0",
+    "url": "/WeatherApi/static/media/rain-clouds.02d4abae.jpg"
+  },
+  {
+    "revision": "d642f171fd8f1e81ef2d",
+    "url": "/WeatherApi/static/js/main.daab312c.chunk.js"
+  },
+  {
+    "revision": "bb920652f61316f8ceb7a826fd14b337",
+    "url": "/WeatherApi/static/media/storm.bb920652.jpg"
+  },
+  {
+    "revision": "c4e0f6d77ac326d662a95160ae485d69",
+    "url": "/WeatherApi/static/media/fog.c4e0f6d7.jpg"
+  },
+  {
+    "revision": "b3fe923d9bc9848635b4733e2914cbc0",
+    "url": "/WeatherApi/static/media/snow.b3fe923d.jpg"
+  },
+  {
+    "revision": "621421a7b3b530f249fe5258f7da827d",
+    "url": "/WeatherApi/static/media/night-no-clouds.621421a7.jpg"
+  },
+  {
+    "revision": "bb230197eb2f1e686b7affad9d7c038b",
+    "url": "/WeatherApi/static/media/night-few-clouds.bb230197.jpg"
+  },
+  {
+    "revision": "d8407dbc4d1b0ad31d1270c85aaaac33",
+    "url": "/WeatherApi/static/media/night-storm.d8407dbc.jpg"
+  },
+  {
+    "revision": "c812f8579fb5304a56672d761db98ba5",
+    "url": "/WeatherApi/static/media/night-snow.c812f857.jpg"
+  },
+  {
+    "revision": "2ab0f188fd4064b2becec16d3c9fceb0",
+    "url": "/WeatherApi/static/media/night-fog.2ab0f188.jpg"
+  },
+  {
+    "revision": "ba5f5e396481b844e42bca064e9b49ed",
+    "url": "/WeatherApi/index.html"
+  }
+];
